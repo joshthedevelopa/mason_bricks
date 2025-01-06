a57a5b0345e36fa4547800728c5f5939c382aa0c
Joshua Tetteh <joshua.nanakojo.tetteh@gmail.com>
2025-01-06 12:04:03 +0000

    chore: Improve default contents

--- clean_architect/__brick__/lib/main.dart
+++ clean_architect/__brick__/lib/main.dart
@@ -1,1 +1,20 @@
+import 'imports.dart';
 
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Main App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Material(),
+    );
+  }
+}
