49713004c91433cc50892537b482526b92fbeb7f
Joshua Tetteh <joshua.nanakojo.tetteh@gmail.com>
2025-01-07 17:10:11 +0000

    feat: add initial LICENSE and CHANGELOG files for clar_ui, clar_data, clar_setup, and clar_domain

--- clar/hooks/post_gen.dart
+++ clar/hooks/post_gen.dart
@@ -1,11 +1,6 @@
 import "dart:io";
 import "package:mason/mason.dart";
 
-const greenCheck = "\x1B[32m\u2714\x1B[0m";
-const git = "https://github.com/joshthedevelopa/mason_bricks.git";
-const option = const ProgressOptions();
-final directoryTarget = DirectoryGeneratorTarget(Directory.current);
-
 Future<void> run(HookContext context) async {
   final name = context.vars["name"];
 
@@ -32,98 +27,4 @@ Future<void> run(HookContext context) async {
   }
 
   await build(context);
-}
-
-Future<void> generate(
-  HookContext context,
-  String path, [
-  Map<String, dynamic> vars = const {},
-]) async {
-  context.logger.info("");
-  final generationProgress = context.logger.progress(
-    "Generating ${path.split("_").lastOrNull ?? "clar"} files",
-    options: ProgressOptions(),
-  );
-
-  final brick = Brick.path("parts/$path");
-  final generator = await MasonGenerator.fromBrick(brick);
-  final files = await generator.generate(directoryTarget, vars: vars);
-
-  generationProgress.complete();
-
-  for (final file in files) {
-    final fileName = file.path.replaceAll("\\", "/");
-    context.logger.info(
-      "$greenCheck Generated "
-      "${fileName.replaceAll(RegExp(".*lib\/"), "")} - ${file.status.name}",
-    );
-  }
-}
-
-Future<void> installPackages(HookContext context) async {
-  final installProgress = context.logger.progress(
-    "Installing packages.",
-    options: ProgressOptions(),
-  );
-
-  await Process.run(
-    "flutter",
-    [
-      "pub",
-      "add",
-      "flutter_bloc",
-      "freezed_annotation",
-      "json_annotation",
-      "go_router"
-    ],
-    runInShell: true,
-  );
-  await Process.run(
-    "flutter",
-    [
-      "pub",
-      "add",
-      "--dev",
-      "build_runner",
-      "build_verify",
-      "freezed",
-      "json_serializable",
-      "go_router_builder"
-    ],
-    runInShell: true,
-  );
-
-  await Process.run(
-    "flutter",
-    ["pub", "get"],
-    runInShell: true,
-  );
-
-  installProgress.complete();
-}
-
-Future<void> build(HookContext context) async {
-  final buildProgress = context.logger.progress(
-    "Building project.",
-    options: option,
-  );
-
-  final buildYaml = File("build.yaml");
-  if (await buildYaml.exists()) {
-    String content = await buildYaml.readAsString();
-    if (!RegExp("{{dir}}").hasMatch(content)) {
-      content = content.replaceAll("{dir}", "{{dir}}");
-      content = content.replaceAll("{file}", "{{file}}");
-
-      await buildYaml.writeAsString(content);
-    }
-  }
-
-  await Process.run(
-    "dart",
-    ["run", "build_runner", "build", "-d"],
-    runInShell: true,
-  );
-
-  buildProgress.complete();
 }
---
+++ tools/mason_function_extensions/lib/src/mason_function_extensions_base.dart
@@ -0,0 +1,101 @@
+import 'dart:io';
+import 'package:mason/mason.dart';
+
+const _greenCheck = "\x1B[32m\u2714\x1B[0m";
+const _git = "https://github.com/joshthedevelopa/mason_bricks.git";
+const _defaultOptions = const ProgressOptions();
+final _defaultDirectoryTarget = DirectoryGeneratorTarget(Directory.current);
+
+Future<void> generate(
+  HookContext context,
+  String path, [
+  Map<String, dynamic> vars = const {},
+]) async {
+  context.logger.info("");
+  final generationProgress = context.logger.progress(
+    "Generating ${path.split("_").lastOrNull ?? "clar"} files",
+    options: ProgressOptions(),
+  );
+
+  final brick = Brick.git(GitPath(_git, path: path));
+  final generator = await MasonGenerator.fromBrick(brick);
+  final files = await generator.generate(_defaultDirectoryTarget, vars: vars);
+
+  generationProgress.complete();
+
+  for (final file in files) {
+    final fileName = file.path.replaceAll("\\", "/");
+    context.logger.info(
+      "$_greenCheck Generated "
+      "${fileName.replaceAll(RegExp(".*lib\/"), "")} - ${file.status.name}",
+    );
+  }
+}
+
+Future<void> installPackages(HookContext context) async {
+  final installProgress = context.logger.progress(
+    "Installing packages.",
+    options: ProgressOptions(),
+  );
+
+  await Process.run(
+    "flutter",
+    [
+      "pub",
+      "add",
+      "flutter_bloc",
+      "freezed_annotation",
+      "json_annotation",
+      "go_router"
+    ],
+    runInShell: true,
+  );
+  await Process.run(
+    "flutter",
+    [
+      "pub",
+      "add",
+      "--dev",
+      "build_runner",
+      "build_verify",
+      "freezed",
+      "json_serializable",
+      "go_router_builder"
+    ],
+    runInShell: true,
+  );
+
+  await Process.run(
+    "flutter",
+    ["pub", "get"],
+    runInShell: true,
+  );
+
+  installProgress.complete();
+}
+
+Future<void> build(HookContext context) async {
+  final buildProgress = context.logger.progress(
+    "Building project.",
+    options: _defaultOptions,
+  );
+
+  final buildYaml = File("build.yaml");
+  if (await buildYaml.exists()) {
+    String content = await buildYaml.readAsString();
+    if (!RegExp("{{dir}}").hasMatch(content)) {
+      content = content.replaceAll("{dir}", "{{dir}}");
+      content = content.replaceAll("{file}", "{{file}}");
+
+      await buildYaml.writeAsString(content);
+    }
+  }
+
+  await Process.run(
+    "dart",
+    ["run", "build_runner", "build", "-d"],
+    runInShell: true,
+  );
+
+  buildProgress.complete();
+}
