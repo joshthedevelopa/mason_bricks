7afb3876ea7a2d6475122647ed8787d512e0b871
Joshua Tetteh <joshua.nanakojo.tetteh@gmail.com>
2025-01-07 16:07:07 +0000

    fix: correct build.yaml path formatting and comment out unused variable assignments in pre_gen hook

--- clar_setup/hooks/pre_gen.dart
+++ clar_setup/hooks/pre_gen.dart
@@ -1,9 +1,9 @@
 import "package:mason/mason.dart";
 
 void run(HookContext context) {
-  context.vars = {
-    ...context.vars,
-    "dir": "{{ dir }}",
-    "file": "{{ file }}",
-  };
+  // context.vars = {
+  //   ...context.vars,
+  //   "dir": "{{ dir }}",
+  //   "file": "{{ file }}",
+  // };
 }
